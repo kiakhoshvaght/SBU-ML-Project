f6b2521cb71767ea80650659525f586c46ad9cd9
kia <kiakhoshvaght@gmail.com>
2023-02-05 00:14:37 +0330

    data added and license removed.

--- clustering.py
+++ clustering.py
@@ -8,7 +8,7 @@ from trackml.dataset import load_event, load_dataset
 from trackml.score import score_event
 
 # Change this according to your directory preferred setting
-path_to_train = "../data/train_1"
+path_to_train = "data/train_1"
 
 # This event is in Train_1
 event_prefix = "event000001000"
